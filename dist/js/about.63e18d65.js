cb6c7565f3fc751ce3bbad0448bd6fc95c787757
oLejikOnMyEvent <g3tf1ght@gmail.com>
2019-02-19 18:49:22 +0300

    second

---
+++ dist/js/about.63e18d65.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",[n("h1",[t._v(" about page")]),n("button",{staticClass:"btn btn-success",attrs:{type:"button"}},[t._v("Success")])])}],c=n("2877"),a={},r=Object(c["a"])(a,s,u,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.63e18d65.js.map
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,19 +9,16 @@
            </div>
        
            
-            <div class="skills">
-              <b-card>
-           <h2> Навыки :</h2>
-          Имел опыт работы с фреймворками: Vue, VueCli (spa), Laravel 5.3 (CRUD)
-             Опыт работы с базами данных (SQL запросы, нормализация форм, проектирование БД )
-             Имел опыт работы с  инструментами:
-            Препроцессоры: Pug (Jade), SASS
-            UX/UI:  bootstrap 3, Figma(аналог sketch), Photoshop
-            Сборщики: Gulp, Webpack
-          Пакетный менеджер: Npm
-            Система контроля версий: Git
-            
-              </b-card>
+            <div class="main">
+
+ <b-card>
+ <h2> Осипов Олег Игоревич </h2>
+            <h3> 25 лет, Родной город Кольчугино</h3>
+            <h3>Высшее образование Университет Дубна: САУ, ИВТ</h3>
+            <h3>6 курс магистратуры</h3>
+         </b-card>
+
+              
                 <a href="https://github.com/oLejikOnMyEvent" target="_blank" > <img src="../assets/linkLogo/git.png" class="gitPhoto"> </a>
        <a href="https://vk.com/sl0wowow" target="_blank"> <img src="../assets/linkLogo/vk.png"  class="vkPhoto"></a>
       </div>
@@ -32,25 +29,28 @@
       <b-col cols="12" lg="6">
      
       <div class="text-about" cols="12" lg="6">
-         <b-card>
- <h2> Осипов Олег Игоревич </h2>
-            <h3> 25 лет, Родной город Кольчугино</h3>
-            <h3>Высшее образование Университет Дубна: САУ, ИВТ</h3>
-            <h3>6 курс магистратуры</h3>
-         </b-card>
+        <b-card>
+           <h2> Навыки :</h2>
+        <li> <b> Имел опыт работы с фреймворками:</b> Vue, VueCli (spa), Laravel 5.3 (CRUD), Nuxt.js </li>
+            <li><b> Опыт работы с базами данных</b> (SQL запросы, google Fibebase)</li>
+           <li><b> Препроцессоры:</b> Pug (Jade), SASS</li>
+          <li><b>  UX/UI:</b>  bootstrap 3-4, Figma(аналог sketch), Photoshop, Veutify</li>
+          <li> <b> Сборщики:</b> Gulp, Webpack</li>
+         <li><b> Пакетный менеджер:</b> Npm</li>
+           <li><b> Система контроля версий:</b> Git</li>
+            
+              </b-card>
 
                <b-card>
             <h2>Знания:</h2>
              html + css 
-                    поверхностные знания Javascript + теоретические основы ES 6
+                  знания Javascript ES5 + основы ES 6
             Понимание основных принципов ооп (инкапсуляция, полиморфизм, наследование).   
-                 Понимание принципов MVC   
+                 Понимание паттерна MVC   
+                 Английский язык – разговорный на уровне pre- intermediate, технический
                     </b-card> 
       </div>
 
- 
-
-       
      </b-col>
       </b-row>
       
@@ -72,8 +72,11 @@ export default {
 </script>
 
 <style scope>
+  li {
+    list-style-type: none;
+   }
 .text-about{
-  padding-top: 129px;
+  padding-top: 81px;
 }
 @media screen and (max-width: 1024px) {
   .text-about{ padding-top: 2px;}
@@ -92,8 +95,8 @@ export default {
 width: 120px;
 
 }
-.skills{
-  margin-top: 50px;
+.main{
+  margin-top: 74px;
 }
 </style>
 
